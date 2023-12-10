8a0798ef0481d40857f8004c46fc4aa406f44e94
Misachka <136197530+Misachka@users.noreply.github.com>
2023-12-10 17:42:49 -0500

    started working on client side, updated package json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({ uri: 'http://localhost:3001/graphql'}); //link for Apollo Client
+
+//authentication link to attach token to request headers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer: ${token}` : "",
+    },
+  };
+});
+
+//ApolloClient and cache
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
-    <>
+    
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+      </ApolloProvider>
+    
   );
 }
 
